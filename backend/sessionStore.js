887cc1aabf91f124b302159f926d4ec54fb3fc2c
Russell Heines <russellheines@gmail.com>
2024-06-23 16:56:51 -0400

    Deleting old version

--- backend/sessionStore.js
+++
@@ -1,9 +0,0 @@
-const { Firestore } = require('@google-cloud/firestore');
-const { FirestoreStore } = require('@google-cloud/connect-firestore');
-
-const sessionStore = new FirestoreStore({
-    dataset: new Firestore(),
-    kind: 'express-sessions',
-});
-
-module.exports = sessionStore;
--- frontend/src/components/Practice.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Practice = () => {
-    return <h2>Under construction!</h2>;
-  };
-  
-export default Practice;
